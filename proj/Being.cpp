acc6fa02699a5358567150d8e9d9bcfce3def4d2
Paweł <pwalig03@gmail.com>
2023-11-05 22:48:53 +0100

    Provide Being reading functionality

--- proj/Being.cpp
+++ proj/Being.cpp
@@ -1,12 +1,76 @@
 #include "Being.hh"
 #include <iostream>
+
 using namespace std;
 
 
+Being* Being::GetPointerFromStream(ifstream & _ifstream){
+    int t;
+    _ifstream >> t;
+    Being* b;
+    switch (t)
+    {
+    case 0:
+        b = new Being();
+        break;
+        
+    case 1:
+        b = new BasicBeing();
+        break;
+        
+    case 2:
+        b = new Living();
+        break;
+    
+    default:
+        break;
+    }
+    b->Read(_ifstream);
+    return b;
+}
+
+
+// ----------Being----------
+
 void Being::Act() {}
+
+void Being::Read(ifstream & _ifstream) {
+    _ifstream >> x;
+    _ifstream >> y;
+}
+
+void Being::PrintInfo(){
+    cout << "x: " << this->x << ", y: " << this->y << endl;
+}
+
+
+// ----------Living----------
 
 void Living::Act() {}
 
-void BasicBeing::Act(){
-    cout << "Yeeeeeeeet";
+void Living::Read(ifstream & _ifstream) {
+    this->Being::Read(_ifstream);
+    _ifstream >> hp;
+}
+
+void Living::PrintInfo() {
+    cout << "hp: " << this->hp << ", ";
+    this->Being::PrintInfo();
+}
+
+
+// ----------BasicBeing----------
+
+void BasicBeing::Act() {
+    cout << "Basic Being!!!" << this-> value << endl;
+}
+
+void BasicBeing::Read(ifstream & _ifstream) {
+    this->Being::Read(_ifstream);
+    _ifstream >> value;
+}
+
+void BasicBeing::PrintInfo(){
+    cout << "value: " << this->value << ", ";
+    this->Being::PrintInfo();
 }
--- proj/Being.hh
+++ proj/Being.hh
@@ -1,17 +1,39 @@
 #pragma once
 
+#include <fstream>
+using namespace std;
+
 class Being{
+    protected:
+    int x;
+    int y;
+
+    virtual void Read(ifstream & _ifstream);
+
     public:
     virtual void Act();
+    virtual void PrintInfo();
+    
+    static Being* GetPointerFromStream(ifstream & _ifstream);
 };
 
 class Living : public Being{
+    protected:
     int hp = 0;
+    virtual void Read(ifstream & _ifstream);
+
     public:
     virtual void Act();
+    virtual void PrintInfo();
 };
 
 class BasicBeing : public Being{
+    int value = 0;
+
+    protected:
+    virtual void Read(ifstream & _ifstream);
+
     public:
     virtual void Act();
+    virtual void PrintInfo();
 };
